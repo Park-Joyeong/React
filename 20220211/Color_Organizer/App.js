0787ea585dc6047463594c44273955efa4098ad4
Park Joyeong <drhamst@gmail.com>
2022-02-12 21:35:15 +0900

    Practice hooks with component tree

--- 20220211/Color_Organizer/App.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import colorData from "./components/colorList/color-data.json";
-import ColorList from "./components/colorList/ColorList.js";
-
-export default function App() {
-    const [colors] = useState(colorData);
-    return <ColorList colors={colors} />;
-}
---
+++ 20220211/Color_Organizer/src/components/colorList/AddColorForm.js
@@ -0,0 +1,10 @@
+import React, { useRef } from "react";
+
+export default function AddColorForm({ onNewColor = f => f }) {
+    const txtTitle = useRef();
+    const hexColor = useRef();
+
+    const submit = e => { }
+
+    return;
+}
