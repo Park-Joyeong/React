ab224c5ab00645dc8f2b077f98bc8ee4b2b132b1
Park Joyeong <drhamst@gmail.com>
2022-02-21 13:00:29 +0900

    20220221

--- 20220217/src/App.js
+++ 20220217/src/App.js
@@ -1,14 +1,49 @@
-import { useEffect } from "react";
-import useAnyKeyToRender from "./components/useAnyKeyToRender";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 function App() {
-  useAnyKeyToRender();
-
   useEffect(() => {
-    console.log("fresh render");
+    console.log("useEffect");
+  });
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect");
   });
 
-  return <h1>Open the console</h1>;
+  return <div>ready</div>;
+}
+
+function useWindowSize() {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  const resize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("resize", resize);
+    resize();
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
+  return [width, height];
+};
+
+function useMousePosition() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const setPosition = ({ x, y }) => {
+    setX(x);
+    setY(y);
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("mousemove", setPosition);
+    return () => window.removeEventListener("mousemove", setPosition);
+  }, []);
+
+  return [x, y];
 }
 
 export default App;
